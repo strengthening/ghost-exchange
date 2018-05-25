ba94751708286478eefe47e1a10b4a15bcbaf44d
strengthening <ducg@foxmail.com>
2018-05-25 10:41:32 +0800

    add feature error type

---
+++ error/okex.js
@@ -0,0 +1,7 @@
+module.exports = {
+  FUTURE: {
+    ERR_SYSTEM_ERROR: 20014, //  system error
+    ERR_ORDER_NOT_EXIST: 20015, //  order does not exist
+    ERR_REQUEST_TOO_FREQUENCY: 20049, //  Request frequency too high
+  },
+};
